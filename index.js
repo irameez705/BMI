c954812190b9f648e104fb434d55a2a6dd587393
irameez705 <155886868+irameez705@users.noreply.github.com>
2024-03-12 02:15:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightInKg = 70; // 70kg
+let heightInMeters = 1.75; // 1.75m
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`your bmi is${bmi}`);
---
+++ index.ts
@@ -0,0 +1,5 @@
+//BMI Calculator
+let weightInKg = 70 ; // 70kg
+let heightInMeters = 1.75; // 1.75m
+let bmi = weightInKg / (heightInMeters * heightInMeters)
+console.log(`your bmi is${bmi}`);
